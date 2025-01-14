94fb60c27fa505f299cc61048b7dcc6161baf177
GodRKOp <79527911+ranadipto@users.noreply.github.com>
2025-01-14 22:06:32 +0530

    Update Json.jsx
    adding some info

--- Json.jsx
+++ Json.jsx
@@ -1,13 +1,16 @@
+//basic json convertion of any value with localstorage 
+
+
 function App() {
   return (
     <div>
       Hello from App.js
       <button
         onClick={() => {
-          localStorage.setItem("arr", JSON.stringify([1, 2, 3, 4]));
+          localStorage.setItem("arr", JSON.stringify([1, 2, 3, 4]));    //"arr" means the variable or key name of the localstorage value JSON.stringify converts the array into string
           localStorage.setItem(
             "obj",
-            JSON.stringify({ name: "name", address: "address1" })
+            JSON.stringify({ name: "name", address: "address1" })       
           );
         }}
       >
@@ -16,7 +19,7 @@ function App() {
       <button
         onClick={() => {
           let arr = localStorage.getItem("arr");
-          console.log(JSON.parse(arr));
+          console.log(JSON.parse(arr));                     //"JSON.parse" converts the already converted stringify value into its original form 
           let obj = localStorage.getItem("obj");
           console.log(JSON.parse(obj));
         }}
